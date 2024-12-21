83f5652a1657402552ca343f150a7ea2f040cadb
Pau Enciso Bosch <pauenbosch@gmail.com>
2024-12-21 01:43:13 +0100

    Change: Updated navigation for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './styles.css'
 import App from './App.jsx'
 import AuthProvider from './auth/AuthProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/anitrack-app">
       <AuthProvider>
         <App />
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
